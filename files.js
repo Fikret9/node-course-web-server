c904164c507b1c1854217443b72c05c3c53cbbec
fikret <f_tokyay@yahoo.com>
2018-03-24 12:08:56 -0500

    commit

--- files.js
+++ files.js
@@ -1,31 +1,18 @@
 const express = require('express');
-const hbs = require('hbs');
-const fs = require('fs');
-var app = express();
+const hbs     = require('hbs');
+const fs      = require('fs');
+var app       = express();
 const bodyparser = require("body-parser");
-var folder = __dirname;
+var folder ;
 var filename = ' ';
 app.use(bodyparser.urlencoded({extended:true}));
-hbs.registerHelper("if", function(conditional, options) {
-  if (options.hash.desired === options.hash.type) {
-    options.fn(this);
-  } else {
-    options.inverse(this);
-  }
-});
-var fetchFile = () => {
-  try {
-    var fileString = fs.readFileSync(filename,{ encoding: 'utf8' });
-    return (fileString);
-  }
-  catch (e) {
-      console.log(e);
-      return [];
-  }
-}
+
 folder = __dirname ;
 app.set('view engine','hbs');
+
 app.get('/',(req,res) =>{
+  console.log(" i am  here");
+  console.log(req.body.directoryInput);
   res.render('homeFiles.hbs',{
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to My File Utility',
@@ -33,23 +20,74 @@ app.get('/',(req,res) =>{
   });
 });
 
+
 app.post('/',(req,res) =>{
-   console.log("REQUEST OBJECT:");
-   console.log(req.body);
    dirName = req.body.directoryInput;
-   console.log(req.body.directoryInput);
    res.redirect("/dir");
 });
 
-
 app.get('/dir',(req,res) =>{
-   filesInDirectory = dirName;
+  var folder = dirName  ;
+  var filesInDirectory = [];
+  filesInDirectory = getDirectoryContent(folder);
+
    res.render('dir.hbs',{
      pageTitle: 'Directory Listing',
      welcomeMessage: 'Directory Listing',
      dirname: dirName,
      directory : filesInDirectory
    });
+});
+
+app.get('/dir/:id',(req,res) =>{
+  console.log(req.params.id);
+  dirName = dirName+'/'+req.params.id.trim();
+  dirName.replace('/','\'');
+    console.log('Dirname: '+ req.params.id);
+  filesInDirectory = getDirectoryContent(dirName);
+  res.render('dir.hbs',{
+    pageTitle: 'Directory Listing',
+    welcomeMessage: 'Directory Listing',
+    dirname: dirName,
+    directory : filesInDirectory
+  });
+});
+
+
+app.post('/dir',(req,res) =>{
+   var selectedFilesToCombine = req.body.selectedfiles.split(",");
+  for (var i=0;i<selectedFilesToCombine.length;i++){
+       var fileElement = selectedFilesToCombine[i].trim();
+       var noteString = readFile(dirName+'/'+fileElement);
+       if (noteString) {
+        writefile(noteString);
+       }
+       res.redirect("/show");
+   }
+});
+
+app.get('/show',(req,res) =>{
+   var textcontent = readFile('combined.txt');
+   if (!textcontent) {
+     res.redirect('/dir');
+   }
+   else
+   {
+    var content = textcontent.split("\u000A");
+    res.render('show',{
+     pageTitle: 'Combined file',
+     welcomeMessage: 'Combined file',
+     content : content
+   });
+   }
+});
+
+app.post('/del',(req,res) =>{
+  fs.unlink('combined.txt', function (err) {
+   if (err) {
+     res.redirect("/dir");
+   }
+ });
 });
 
 app.get('/file',(req,res) =>{
@@ -63,44 +101,33 @@ app.listen(3100, () => {
 });
 
 
-if (filename) {
-  console.log('Filename:' + filename);
-  var result = [];
-  result = fetchFile();
-  console.log(result);
+function writefile(fileString){
+  fs.appendFileSync('combined.txt',fileString);
 }
 
 
-var folder = 'C:\\Users\\Fikret\\Desktop'  ;
-console.log('Starting directory: '+folder);
-var filesInDirectory = [];
-
-function readdirectory
-fs.readdir(folder,(err,files) => {
-  if(files){
-  files.forEach (file => {
-    filesInDirectory.push(
-    {
-      filename: file,
-      filetype : (file.indexOf('.')>-1)?'     ':'     Folder'
-    });
-
-  //  }
-    console.log(">> "+ JSON.stringify(file) );
-
-  });
-}
-});
-
-
-
-var fetchFile = () => {
+function readFile(filename){
   try {
-    var fileString = fs.readFileSync(filename);
-    return JSON.parse(noteString);
+  var filecontent = fs.readFileSync(filename,'utf8');
   }
-  catch (e) {
-      console.log(e);
-      return [];
+  catch (e){
+    console.log(e);
   }
+  return filecontent;
+}
+
+function getDirectoryContent(folder){
+  console.log("folder: " + folder);
+ var filesInDirectory = [];
+ fs.readdir(folder,(err,files) => {
+   if(files){
+   files.forEach (file => {
+     filesInDirectory.push(
+     { filename: file,
+       filetype : (file.indexOf('.')>-1)?null:'     Folder'
+     });
+   });
+ }
+ });
+ return filesInDirectory;
 }
